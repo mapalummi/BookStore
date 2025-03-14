9b5524aa7a30d250e3b87fa8d64db0ef9412ed6a
Marco <marco.palummieri@gmail.com>
2025-03-14 21:22:34 +0100

    add new style to book containera

--- script.js
+++ script.js
@@ -4,22 +4,3 @@ function renderBooks(){
         document.getElementById('book_container').innerHTML += getBookTemplate(i);
     }
 }
-
-
-//   for (let i = 0; i < books.length; i++) {
-//   }
-
-// zeigt die Arrays an:
-// console.log(Object.entries(books));
-  
-// zeigt den Titel an Index-Position 0 an:
-// console.log(books[1].title);
-
-// for (let i = 0; i < books.length; i++) {
-//     console.log(books[i].title);
-//     console.log(books[i].author);
-//     console.log(books[i].likes);
-//     console.log(books[i].price);
-//     console.log(books[i].publishedYear);
-//     console.log(books[i].genre);
-//   }
